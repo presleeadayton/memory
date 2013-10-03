40c2d964a193a3c0abe8ab89ace36e94cae8d3b2
vickirammell <vickirammell@10.17.165.14>
2013-10-03 12:03:10 -0600

    committing

--- src/memory/Memory.java
+++ src/memory/Memory.java
@@ -4,16 +4,38 @@
  */
 package memory;
 
+import java.util.Scanner;
+
 /**
  *
  * @author vickirammell
  */
 public class Memory {
-
-    /**
-     * @param args the command line arguments
-     */
+    
+    //Instance variables
+    String name;
+    String instructions = "This is the game of Memory! \n\n"
+            + "Players take turn, one at a time.\n" +
+"On each turn, a player turns over two cards (one at a time) and keeps them if they match.\n" +
+"If they successfully match a pair of numbers, that player also gets to take another turn.\n" +
+"Players continue taking turns until the last match is found.\n" +
+"Each player then adds up the pairs/matches they got. Each pair is worth 1 point.\n" +
+"The player with the most pairs/matches; WINS.";
     public static void main(String[] args) {
-        // TODO code application logic here
+        Memory myGame = new Memory();
+        myGame.getName();
+        myGame.displayHelp();
+        start game = new start();
+        game.startGame();
+    }
+    public void getName(){ 
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter your name: ");
+        this.name = input.next();
+        
+    }
+    public void displayHelp() {
+         System.out.println("\nWelcome " + this.name + "\n");
+         System.out.println(this.instructions);
     }
 }
---
+++ src/memory/start.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package memory;
+
+import javax.swing.JButton;
+
+/**
+ *
+ * @author presleeadayton
+ */
+public class start {
+    
+
+public void startGame(){ 
+    JButton button2 = new JButton("Start");
+    }
+}
